9a99eae2de63df0df4ff3645eabbce720b590218
Bartosz Kalinowski <kelostrada@gmail.com>
2021-10-21 13:55:31 +0200

    1.4.0 release

--- bulkeditor.php
+++ bulkeditor.php
@@ -36,7 +36,7 @@ class Bulkeditor extends Module
     {
         $this->name = 'bulkeditor';
         $this->tab = 'administration';
-        $this->version = '1.3.1';
+        $this->version = '1.4.0';
         $this->author = 'Kelostrada';
         $this->need_instance = 0;
 
